d6848be97c77073e8d37089d2360322b61afbd67
zoodigital7 <zoo.york4130@gmail.com>
2020-07-31 09:40:38 -0500

    Building out the login form and adding styling for overiding buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,9 @@ import { LoginComponent } from './login/login.component';
     AppRoutingModule,
     NgbModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [
   ],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
+export class LoginComponent implements OnInit, OnDestroy {
+  loginForm: FormGroup
+  loginFormValues: any
+  submitting = false
+  hasError = false
+  errorMsg: string
+  private subs = new Subscription()
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.createFormControls()
+    this.createForm()
+  }
+
+  createFormControls() {
+    this.loginFormValues = {
+      email: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+    }
+  }
+
+  createForm() {
+    this.loginForm = this.fb.group(this.loginFormValues);
+  }
+
+  submitForm() {
+
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe()
   }
 
 }
