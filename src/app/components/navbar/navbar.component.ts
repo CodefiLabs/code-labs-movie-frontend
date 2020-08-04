2f2a950136fd0866ff55137ba1d0e7af97f8ccc2
zoodigital7 <zoo.york4130@gmail.com>
2020-08-04 16:33:18 -0500

    Setup subscriptions and sub to currentUser in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from 'src/app/shared/models/user';
+import { UserService } from 'src/app/shared/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
+export class NavbarComponent implements OnInit, OnDestroy {
+  currentUser: User
+  private subs = new Subscription()
+  constructor(
+    private userService: UserService
+  ) {
+    this.currentUser = this.userService.currentUserValue // gets the currentUser value from the userService
+  }
 
   ngOnInit(): void {
+  }
+
+  subscribeToCurrentUser() {
+    this.subs.add( // creates a new subscription
+      this.userService.currentUser.subscribe(user => { // creates the subscription
+        if (user) { // if the logged in users exists set that value in this component
+          this.currentUser = user
+        } else { // else set the current user as null
+          this.currentUser = null
+        }
+      })
+    )
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe() // destroy all subscriptions to prevent memory leak
   }
 
 }
