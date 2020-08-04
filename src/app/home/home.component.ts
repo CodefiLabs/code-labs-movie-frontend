96ae27d60a39b0815d6b3b9a643ef8de60dda2aa
zoodigital7 <zoo.york4130@gmail.com>
2020-08-04 17:30:39 -0500

    Import and define all services, subs, and methods in home component initially

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { User } from '../shared/models/user';
 import { UserService } from '../shared/services/user.service';
 import { MovieService } from '../shared/services/movie.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   private currentUser: User
+  private subs = new Subscription()
   constructor(
+    private router: Router,
     private userService: UserService,
     private movieService: MovieService
   ) {
@@ -18,6 +22,42 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.handleSubs()
+  }
+
+  handleSubs() {
+    this.subToCurrentUser()
+    this.retrieveAllMovies()
+  }
+
+  subToCurrentUser() {
+    this.subs.add(
+      this.userService.currentUser.subscribe(user => {
+        if (user) {
+          this.currentUser = user
+        } else {
+          this.currentUser = null
+        }
+      })
+    )
+  }
+
+  retrieveAllMovies() {
+    this.subs.add( // adds a new subscription to the array
+      this.movieService.getAllMovies().subscribe(data => { // get all movies
+        if (data) {
+          debugger
+        }
+      }, error => {
+        if (error) {
+          console.error(error)
+        }
+      })
+    )
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe() // Destroy all subs to prevent memory leak
   }
 
 }
