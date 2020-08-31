9d78b938adb465f8f9f962ab7344bec73802a2a2
Zoodigital7 <zoo.york4130@gmail.com>
2020-08-31 13:14:04 -0500

    Add MustMatch Validator to resources

--- src/app/resource-pages/day-four/day-four.component.ts
+++ src/app/resource-pages/day-four/day-four.component.ts
@@ -327,6 +327,29 @@ export class LoginComponent implements OnInit, OnDestroy {
   body { margin: 0; font-family: Roboto, "Helvetica Neue", sans-serif; }
 
   `
+
+  mustMatchValidator = `
+  import { FormGroup } from '@angular/forms';
+
+  // custom validator to check that two fields match
+  export function MustMatch(controlName: string, matchingControlName: string) {
+      return (formGroup: FormGroup) => {
+          const control = formGroup.controls[controlName];
+          const matchingControl = formGroup.controls[matchingControlName];
+          if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+              // return if another validator has already found an error on the matchingControl
+              return;
+          }
+          // set error on matchingControl if validation fails
+          if (control.value !== matchingControl.value) {
+              matchingControl.setErrors({ mustMatch: true });
+          } else {
+              matchingControl.setErrors(null);
+          }
+      }
+  }
+
+  `
   constructor(
     private http: HttpClient
   ) { }
