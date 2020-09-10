51743a22199cf405cd640a58af6c5a349c6a93a6
Zoodigital7 <zoo.york4130@gmail.com>
2020-09-10 11:03:12 -0500

    Update resources for day-seven

--- src/app/resource-pages/day-seven/day-seven.component.ts
+++ src/app/resource-pages/day-seven/day-seven.component.ts
@@ -218,7 +218,63 @@ export class DaySevenComponent implements OnInit {
     }
   
   }
-  
+
+  `
+
+  singleReviewCardHtml = `
+
+  <div *ngIf="review" class="row">
+    <div class="col-12 d-inline-flex">
+        <div class="flex">
+            <app-input-star-rating [rating]="review.rating" [starCount]="5" [padding]="0" [size]="24"></app-input-star-rating>
+        </div>
+        <div class="flex ml-2">
+            <p class="review-author mt-2">by {{review.user_nickname}}<span class="ml-2">{{review.updated_at | timeAgo}}</span></p>
+        </div>
+    </div>
+    <div class="col-12">
+        <p class="review-body">{{review.body}}</p>
+    </div>
+  </div>
+
+  `
+
+  singleReviewCardScss = `
+  h1 {
+    font-family: 'Raleway';
+    font-weight: bold;
+    font-size: 24px;
+    color: #777777;
+}
+
+.review-author {
+    font-family: 'Raleway';
+    font-weight: bold;
+    font-size: 16px;
+    color: #777777;
+    span {
+        color: #CCCCCC;
+    }
+}
+
+.review-body {
+    font-family: 'Raleway';
+    font-weight: 500;
+    font-size: 16px;
+    color: #777777;
+}
+
+.read-all-reviews {
+    font-family: 'Raleway';
+    font-weight: bold;
+    font-size: 16px;
+    color: #777777;
+    a {
+        text-decoration: underline;
+        color: #FF4436;
+    }
+}
+
   `
 
   constructor() { }
