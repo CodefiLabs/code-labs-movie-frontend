7715bc4b4475d16d7df9ffafa5abe84d0395d618
Zoodigital7 <zoo.york4130@gmail.com>
2020-10-18 04:11:56 -0500

    Add resources

--- src/app/resource-pages/day-ten/day-ten.component.ts
+++ src/app/resource-pages/day-ten/day-ten.component.ts
@@ -308,8 +308,32 @@ cancel() {
 }
 `
 
-newMovieComponent = `
-
+uploadMovieImg = `
+uploadMovieImage(file, name, accessKey, secretKey) {
+  const buf = new Buffer(file.replace(/^data:image\/\w+;base64,/, ''), 'base64')
+  const bucket = new S3({
+    accessKeyId: accessKey,
+    secretAccessKey: secretKey,
+    region: 'us-east-2',
+  });
+  const params = {
+    Bucket: 'code-labs-one-movie-images',
+    Key: 'images/' + name,
+    Body: buf,
+    ACL: 'public-read',
+    ContentEncoding: 'base64',
+    ContentType: 'image/png'
+  };
+  bucket.upload(params, function (err, data) {
+    if (err) {
+      console.log('There was an error uploading your file: ', err)
+      return { success: false, error: err  }
+    } else {
+      console.log('Successfully uploaded file.', data)
+      return { success: true, file: data.location }
+    }
+  })
+}
 `
   constructor() { }
 
